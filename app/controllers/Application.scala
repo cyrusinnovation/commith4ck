7fe1ea5bc2394503a2b44a7bb0a675543f02f09b
Nicholas Goodwin <theunforgiven@gmail.com>
2014-09-22 16:45:46 -0400

    fix last change and disable heroku deploy

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,8 +5,7 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    //comment to check assumption
-    Ok(views.html.index("Your new application is ready. YAY."))
+    Ok(views.html.index("Your new application is ready."))
   }
 
 }
