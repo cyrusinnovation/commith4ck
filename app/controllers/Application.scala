fd2380c47bbe65deb932e4f811af5ed9e2c13c0b
Nicholas Goodwin <theunforgiven@gmail.com>
2014-09-25 14:05:09 -0400

    fix logging level

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,7 +4,7 @@ import org.slf4j.LoggerFactory
 import play.api.mvc._
 
 object Application extends Controller with CommitsController with LoggingProvider {
-  protected val log = LoggerFactory.getLogger(Application.getClass)
+  protected val log = LoggerFactory.getLogger("application")
 
   def index = Action {
     Ok(views.html.main("Commit H4ck"))
